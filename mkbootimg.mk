3e3e5b86f3f6ff31ccc63681c45dd4fe08f8472d
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2022-06-19 16:54:55 +0200

    Update mkbootimg.mk

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,28 +1,30 @@
-FLASH_IMAGE_TARGET := recovery.tar
-BUILT_RAMDISK_CPIO := $(PRODUCT_OUT)/ramdisk-recovery.cpio
-COMPRESS_COMMAND :=  xz -f -9 -e
+LZMA_BIN := $(shell which lzma)
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
-	$(call pretty,"Target boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
-	$(hide) echo -n "SEANDROIDENFORCE" >> $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
-	@echo "Made boot image: $@"
 
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk)
-	@echo "------- Compressing recovery ramdisk -------"
-	$(hide) $(COMPRESS_COMMAND) "$(BUILT_RAMDISK_CPIO)"
-	@echo "------- Making recovery image -------"
-	$(hide) $(MKBOOTIMG) \
-		--kernel $(recovery_kernel) \
-		--ramdisk $(BUILT_RAMDISK_CPIO).xz \
-		--cmdline "$(BOARD_KERNEL_CMDLINE)" \
-		--base $(BOARD_KERNEL_BASE) \
-		--pagesize $(BOARD_KERNEL_PAGESIZE) \
-		$(BOARD_MKBOOTIMG_ARGS) \
-		-o $(PRODUCT_OUT)/recovery.img
-	$(hide) echo -n "SEANDROIDENFORCE" >> $@
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
+		$(recovery_ramdisk) \
+		$(recovery_uncompressed_ramdisk) \
+		$(recovery_kernel)
+	@echo -e ${CL_CYN}"----- Compressing recovery ramdisk with lzma ------"${CL_RST}
+	rm -f $(recovery_uncompressed_ramdisk).lzma
+	$(LZMA_BIN) $(recovery_uncompressed_ramdisk)
+	$(hide) cp $(recovery_uncompressed_ramdisk).lzma $(recovery_ramdisk)
+	@echo ----- Making recovery image ------
+	$(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	@echo -e ${CL_CYN}"----- Made recovery image -------- $@"${CL_RST}
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
-	@echo "------- Made recovery image: $@ -------"
-	$(hide) tar -C $(PRODUCT_OUT) -H ustar -c recovery.img > $(PRODUCT_OUT)/$(FLASH_IMAGE_TARGET)
-	@echo "------- Made flashable image: $(FLASH_IMAGE_TARGET) -------"
+
+#####################################################################
+######### Compress kernel ramdisk using LZMA LehKeda Edit ###########
+#####################################################################
+
+LZMA_BOOT_RAMDISK := $(PRODUCT_OUT)/ramdisk-lzma.img
+
+$(LZMA_BOOT_RAMDISK): $(BUILT_RAMDISK_TARGET)
+	gunzip -f < $(BUILT_RAMDISK_TARGET) | lzma -e > $@
+
+ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(LZMA_BOOT_RAMDISK)
+	$(call pretty,"Target boot image: $@")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --ramdisk $(LZMA_BOOT_RAMDISK)
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
