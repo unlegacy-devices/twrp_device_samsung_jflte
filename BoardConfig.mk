d9b052b05cb55b189bc16a0d1d2feb87b280768b
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2022-07-22 21:29:41 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 #
 # This file sets variables that control the way modules are built
 # thorughout the system. It should not be used to conditionally
@@ -21,50 +20,38 @@
 # included in a build is to use PRODUCT_PACKAGES in a product
 # definition file).
 #
-
 # Inherit from qcom-common
 #-include device/samsung/qcom-common/BoardConfigCommon.mk
-
 # Inherit from proprietary vendor
 #-include vendor/samsung/jf-common/BoardConfigVendor.mk
-
-CM_PLATFORM_SDK_VERSION := 7	# Required for libf2fs.so
-override TARGET_OUT_VENDOR_SHARED_LIBRARIES = $(TARGET_OUT_SHARED_LIBRARIES)
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
-
-
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
-
 # Platform
 TARGET_BOARD_PLATFORM := msm8960
 TARGET_USES_64_BIT_BINDER := true
-
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := krait
-
 # Boot image
 BOARD_KERNEL_IMAGE_NAME := zImage
 TARGET_KERNEL_SOURCE := kernel/samsung/jf
 TARGET_KERNEL_CONFIG := twrp_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := jf_eur_defconfig
-
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=22 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/jflte/bootimg.mk
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/jflte/mkbootimg.mk
-BOARD_CANT_BUILD_RECOVERY_FROM_BOOT_PATCH := true
 
 # Filesystem
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
@@ -78,12 +65,9 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 367001600
 BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-
 # Toolchain
 #KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/gcc/linux-x86/arm/arm-linux-7.1.x-gnueabi/bin
 #TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-Samsung-linux-gnueabi-
-
-
 # Recovery
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_RECOVERY_DENSITY := hdpi
@@ -91,7 +75,6 @@ TARGET_RECOVERY_DEVICE_DIRS := device/samsung/jflte
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_HAS_DOWNLOAD_MODE := true
-
 # TWRP
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
@@ -106,7 +89,5 @@ TW_INCLUDE_CRYPTO := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_TARGET_USES_QCOM_BSP := true
 TW_USE_TOOLBOX := true
-
 TARGET_OTA_ASSERT_DEVICE := jflte
-
 ALLOW_MISSING_DEPENDENCIES := true
